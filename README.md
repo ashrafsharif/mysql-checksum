a31a1d803187330d48ee7620774492cfc1cd223e
ashrafsharif <acopkirk@gmail.com>
2020-08-03 11:59:22 +0800

    first release

---
+++ README.md
@@ -0,0 +1,117 @@
+# MySQL Checksum Script
+
+This script will compare checksum values for all tables on the local server (where this script is executed) with a remote server.
+
+This script utilizes the `CHECKSUM TABLE` statement and compare it with the output from another server using `diff` command. This script is useful if you want to verify all nodes are indeed in synced especially during a switchover/failover before resuming the application back in production.
+
+# Usage
+
+The script should be running on any node in a replication chain. In the following example architecture, you can run the script on `DB3` to compare it with `DB1` and `DB2`:
+
+```
+1 master, 2 slaves of MariaDB Replication with GTID enabled
+
++-------+                             +-------+
+|  DB1  |------async replication----> |  DB2  |
++-------+                             +-------+
+     \                                      
+      \                                +-------+
+       +-------async replication----> |  DB3  |
+                                       +-------+
+```
+
+Where:
+
+* `DB1`: 192.168.10.101
+* `DB2`: 192.168.10.102
+* `DB3`: 192.168.10.103
+
+## Create database user
+
+For performance matters, this script requires two database users with SELECT privileges (one for localhost via socket and one for TCP/IP connection). Therefore, create two user-host as below:
+
+```
+MariaDB> CREATE USER 'checksum'@'localhost' IDENTIFIED BY 'mypassword';
+MariaDB> GRANT SELECT ON *.* TO 'checksum'@'localhost';
+MariaDB> CREATE USER 'checksum'@'192.168.10.%' IDENTIFIED BY 'mypassword';
+MariaDB> GRANT SELECT ON *.* TO 'checksum'@'192.168.10.%';
+```
+
+** We created two users with same username but different hosts (localhost = socket while 192.168.10.% = pattern matching for all IP under 192.168.10.0/24 network).
+
+## Running the script
+
+Before running the script, make sure the application is not sending any writes to the cluster (otherwise the result will always be inconsistent), or the replication slave must be stopped between two nodes that you want to compare. In this example, we are going to stop the replication slave on `DB3` momentarily:
+
+```
+MariaDB> STOP SLAVE;
+```
+
+Invoke the command by specifying one of the remote server that you want to compare. On `DB3`, run the following command to compare with `DB1`:
+
+```
+ ./checksum.sh 192.168.10.101
+```
+
+Example output:
+
+```
+$ ./checksum.sh 192.168.10.101
+
++====================================================+
+|  This script will compare data on 2 servers and    |
+|  determine whether they are in a consistent state  |
++====================================================+
+
+Generating table list on localhost...
+Generating table list on 192.168.10.101...
+
+Table list comparison
+---------------------
+
+Source table list (localhost):
+sbtest.sbtest1
+sbtest.sbtest2
+sbtest.sbtest3
+sbtest.sbtest4
+sbtest.sbtest5
+
+Target table list (192.168.10.101):
+sbtest.sbtest1
+sbtest.sbtest2
+sbtest.sbtest3
+sbtest.sbtest4
+sbtest.sbtest5
+
+Comparing table list on both servers ...
+Result: Looks good! No difference found.
+
+Checksum comparison
+-------------------
+
+Source checksum (localhost):
+sbtest.sbtest1	4021674442
+sbtest.sbtest2	3375019626
+sbtest.sbtest3	1332937866
+sbtest.sbtest4	309612080
+sbtest.sbtest5	78379596
+
+Target checksum (192.168.10.101):
+sbtest.sbtest1	4021674442
+sbtest.sbtest2	3375019626
+sbtest.sbtest3	1332937866
+sbtest.sbtest4	309612080
+sbtest.sbtest5	78379596
+
+Comparing tables' checksum for both servers ...
+Result: Looks good! No difference found.
+
+Summary:
+Data on both servers are consistent.
+```
+
+Once you get the result, resume the replication again or fix the node if data is not consistent:
+
+```
+MariaDB> START SLAVE;
+```
---
+++ checksum.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+## Compare tables on this MySQL server with a remote MySQL server to determine whether both are in a consistent state
+## Example:
+## $ ./checksum.sh 192.168.10.101
+
+## Remote DB user
+DBUSER=checksum
+DBPASS=mypassword
+# To create a dedicated checksum user:
+# CREATE USER 'checksum'@'192.168.10.%' IDENTIFIED BY 'mypassword';
+# GRANT SELECT ON *.* TO 'checksum'@'192.168.10.%';
+# CREATE USER 'checksum'@'localhost' IDENTIFIED BY 'mypassword';
+# GRANT SELECT ON *.* TO 'checksum'@'locahost';
+##
+
+SOURCE=localhost
+TARGET=$1
+PORT=3306
+
+[ -z $TARGET ] && echo 'Please specify a destination server to compare' && exit 1
+
+SOURCE_CHECKSUM=/tmp/source_checksum
+SOURCE_TABLELIST=/tmp/source_tablelist
+TARGET_CHECKSUM=/tmp/dest_checksum
+TARGET_TABLELIST=/tmp/dest_tablelist
+
+cat /dev/null > $SOURCE_CHECKSUM
+cat /dev/null > $SOURCE_TABLELIST
+cat /dev/null > $TARGET_CHECKSUM
+cat /dev/null > $TARGET_TABLELIST
+
+echo
+echo "+====================================================+"
+echo "|  This script will compare data on 2 servers and    |"
+echo "|  determine whether they are in a consistent state  |"
+echo "+====================================================+"
+echo
+echo "Generating table list on ${SOURCE}..."
+mysql -u$DBUSER -p$DBPASS -h $SOURCE -P $PORT -A -Bse 'SELECT CONCAT(table_schema,".", table_name) FROM information_schema.tables WHERE table_schema NOT IN ("mysql","information_schema","performance_schema","sys")' >> $SOURCE_TABLELIST
+[ $? -ne 0 ] && echo 'Error while generating table list. Please check if the DB credentials are correct.' && exit 1
+
+for TABLE in $(cat $SOURCE_TABLELIST); do
+	mysql -u$DBUSER -p$DBPASS -h $SOURCE -P $PORT -A -Bse "CHECKSUM TABLE $TABLE" >> $SOURCE_CHECKSUM
+done
+
+echo "Generating table list on ${TARGET}..."
+mysql -u$DBUSER -p$DBPASS -h $TARGET -P $PORT -A -Bse 'SELECT CONCAT(table_schema,".", table_name) FROM information_schema.tables WHERE table_schema NOT IN ("mysql","information_schema","performance_schema","sys")' >> $TARGET_TABLELIST
+[ $? -ne 0 ] && echo 'Error while generating table list. Please check if the DB credentials are correct.' && exit 1
+
+for TABLE in $(cat $TARGET_TABLELIST); do
+    mysql -u$DBUSER -p$DBPASS -h $TARGET -P $PORT -A -Bse "CHECKSUM TABLE $TABLE" >> $TARGET_CHECKSUM
+done
+
+echo
+echo "Table list comparison"
+echo "---------------------"
+echo
+echo "Source table list ($SOURCE):"
+cat $SOURCE_TABLELIST
+echo 
+echo "Target table list ($TARGET):"
+cat $TARGET_TABLELIST
+echo
+echo "Comparing table list on both servers ..."
+diff $SOURCE_TABLELIST $TARGET_TABLELIST
+if [ $? -eq 0 ]; then
+	echo -n "Result: "
+	echo -e "\e[32mLooks good! No difference found.\e[39m"
+	TABLELIST_IS_SYNCED=1
+else
+	echo -n "Result: "
+	echo -e "\e[31mDifference found.\e[39m"
+	TABLELIST_IS_SYNCED=0
+fi
+
+echo
+echo "Checksum comparison"
+echo "-------------------"
+echo 
+echo "Source checksum ($SOURCE):"
+cat $SOURCE_CHECKSUM
+echo 
+echo "Target checksum ($TARGET):"
+cat $TARGET_CHECKSUM
+echo
+echo "Comparing tables' checksum for both servers ..."
+diff $SOURCE_CHECKSUM $TARGET_CHECKSUM
+if [ $? -eq 0 ]; then
+	echo -n "Result: "
+        echo -e "\e[32mLooks good! No difference found.\e[39m"
+        TABLES_ARE_SYNCED=1
+else
+	echo -n "Result: "
+        echo -e "\e[31mDifference found.\e[39m"
+        TABLES_ARE_SYNCED=0
+fi
+
+echo
+echo "Summary: "
+if [ $TABLELIST_IS_SYNCED -eq 1 ] && [ $TABLES_ARE_SYNCED -eq 1 ]; then
+	echo -e "\e[32mData on both servers are consistent.\e[39m"
+else
+	echo -e "\e[31mData on both servers are NOT consistent. Please sync it first, otherwise data lost might happen!\e[39m"
+fi
+echo
+
+# Cleaning up temp files
+rm -f $SOURCE_CHECKSUM
+rm -f $SOURCE_TABLELIST
+rm -f $TARGET_CHECKSUM
+rm -f $TARGET_TABLELIST
